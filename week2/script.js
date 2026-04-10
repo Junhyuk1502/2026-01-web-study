3369a95c735c524456b65bb821058c8a560141a6
Junhyuk1502 <junhyuk1502@gmail.com>
2026-04-10 22:50:23 +0900

    week2 assignment complete

---
+++ week2/script.js
@@ -0,0 +1,6 @@
+const cheerButton = document.getElementById('cheerButton');
+
+cheerButton.addEventListener('click', () => {
+  cheerButton.textContent = '준혁의 계획 제출하기!';
+  alert('Wait for my Result!');
+});
